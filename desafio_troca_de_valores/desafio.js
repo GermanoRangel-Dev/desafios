2ab273654ff7e363c5ae1adb02fd368300806acb
GermanoRangel-Dev <grangeldev@gmail.com>
2022-10-06 16:10:30 -0300

    commit 2
    desafio realizado com uma variavel temporaria

---
+++ desafio_troca_de_valores/desafio.js
@@ -0,0 +1,16 @@
+// objetivo do desafio é trocar o valor de duas variáveis diferentes
+// let a = 7  &  let b = 10
+//apos a troca a=10 & b=7
+
+let a = 7
+let b = 10
+let temp = a
+
+console.log("O valor de a é: " + a)
+console.log("O valor de b é: " + b)
+
+a = b
+b = temp
+
+console.log("O valor de a é: " + a)
+console.log("O valor de b é: " + b)
