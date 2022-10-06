1677ae3fbad596bf5b77d9e9f656e41aad843a72
GermanoRangel-Dev <grangeldev@gmail.com>
2022-10-06 15:58:53 -0300

    Commit 1
    2 primeiros desafios

---
+++ desafio_area-da-circunferencia/desafio.js
@@ -0,0 +1,7 @@
+//PI = 3.141592 
+// calculo = PI * raio* raio
+
+const PI = 3.141592
+let raio = 10
+let calcArea = PI * raio * raio
+console.log("O valor da área da circunferencia é: " + calcArea + "m2")
