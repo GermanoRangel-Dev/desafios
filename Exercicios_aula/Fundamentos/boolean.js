420cf3d68b894d7f6eaa614079144059e508a230
GermanoRangel-Dev <grangeldev@gmail.com>
2022-10-18 07:58:23 -0300

    Create boolean.js

---
+++ Exercicios_aula/Fundamentos/boolean.js
@@ -0,0 +1,39 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo)
+
+isAtivo = 1
+console.log(!isAtivo)
+
+console.log('os verdadeiros...')
+console.log(!!3)
+console.log(!!-1)
+console.log(!!" ")
+console.log(!!'teste')
+console.log(!!{})
+console.log([])
+console.log(!!Infinity)
+console.log(!!(isAtivo = true))
+
+console.log('os falsos...')
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
+console.log('para finalizar...')
+console.log(!!('' || null || 0 || " " ))
+
+let nome = 'Miguel'
+console.log(nome || 'Desconhecido')
+
+// Havendo dupla !! a sentença será a colocada se for declarado false, será fale, se for declarado true, será  true
+
+
