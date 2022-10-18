2ed961ce4371e9606e54697b26e2857e35ae3fb0
GermanoRangel-Dev <grangeldev@gmail.com>
2022-10-18 08:55:10 -0300

    Create nullUndefined.js

---
+++ Exercicios_aula/Fundamentos/nullUndefined.js
@@ -0,0 +1,24 @@
+let valor // não inicializada
+console.log(valor)
+
+valor = null // ausência de valor
+console.log(valor)
+//console.log(valor.toString()) // Erro!
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.50
+console.log(produto)
+
+produto.preco = undefined //evite atribuir Undefined
+console.log(!!produto.preco)
+console.log(produto)
+
+produto.preco = null  // sem preço
+console.log(!!produto.preco)
+console.log(produto)
+
+// entre atribuir algo sem valor, use o null, evite de qualquer forma o 
+// undefined
