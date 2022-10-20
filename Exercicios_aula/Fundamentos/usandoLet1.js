a53f676b4ae50d19a9c42349b884f57ba5b9da7f
GermanoRangel-Dev <grangeldev@gmail.com>
2022-10-20 09:25:41 -0300

    commit 4
    vários exemplos

---
+++ Exercicios_aula/Fundamentos/usandoLet1.js
@@ -0,0 +1,10 @@
+let numero = 1;
+console.log(numero);
+{
+  let numero = 2;
+
+  let numero2 = 3;
+  console.log('dentro = ', numero);
+  console.log('dentro = ', numero2);
+}
+console.log('fora = ', numero);
