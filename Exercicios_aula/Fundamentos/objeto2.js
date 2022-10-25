b4d87a24cd207f5764ad57763abbbe31384602b8
GermanoRangel-Dev <grangeldev@gmail.com>
2022-10-25 07:41:11 -0300

    Create objeto2.js

---
+++ Exercicios_aula/Fundamentos/objeto2.js
@@ -0,0 +1,10 @@
+console.log("1 - " + typeof Object);
+console.log("2 - " +typeof new Object);
+
+const Cliente = function(){}
+console.log("3 - " +typeof Cliente);
+console.log("4 - " +typeof new Cliente);
+
+class Produto {}
+console.log("5 - " +typeof Produto);
+console.log("6 - " +typeof new Produto());
