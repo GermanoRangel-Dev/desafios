5248bf80612f1c7c3353841f39189ab886d21243
GermanoRangel-Dev <grangeldev@gmail.com>
2022-10-25 08:13:00 -0300

    Create atribuicao.js

---
+++ Exercicios_aula/Fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7
+let b = 3
+
+b+=a  // b = b + a
+console.log('1 - ' + b)
+
+b-=4  // b = b - a
+console.log('2 - ' + b)
+
+b*= 2  // b = b * 2
+console.log('3 - ' + b)
+
+b/=2  // b = b / 2
+console.log('4 - ' + b)
+
+b%=2  // b = b % 2 chamado resto da divisão 0 ou 1 
+console.log('5 - ' + b)
